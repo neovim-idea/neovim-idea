eb8b6b1d6c5916f28a6755d6c0ff2f7fbb5bca25
Diego Casella <polentino911@gmail.com>
2025-11-18 00:55:59 +0100

    fix: keep catppuccin color scheme change and lualine in sync

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,7 @@ return {
   config = function()
     require("lualine").setup({
       options = {
-        theme = "catppuccin",
+        -- theme = "catppuccin-intellijdark",
         component_separators = { left = "", right = "" },
         section_separators = { left = "", right = "" },
       },

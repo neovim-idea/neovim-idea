61c1405772f0b13144bda5d1cfeb5f9562777650
Diego Casella <polentino911@gmail.com>
2025-11-23 21:44:37 +0100

    fix: make nvim-treesitter's options extensible

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,17 +1,11 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  -- TODO figure out where the new settings went, when moving to "main"
+  -- TODO figure out where the settings went, when moving to "main"
   branch = "master",
   lazy = false,
   config = function()
     local config = require("nvim-treesitter.configs")
-    config.setup({
-      -- auto_install = true -- to autoinstall languages as they're encountered
-      ensure_installed = { "lua", "javascript", "java", "scala" },
-      highlight = { enable = true },
-      indent = { enable = true },
-      fold = { enable = true },
-    })
+    config.setup(require("neovim-idea.options").get_nvim_treesitter_options())
   end,
 }

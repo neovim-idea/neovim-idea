3f0261cc5b5f19a20cfa35f41030c584d05e850c
Diego Casella <polentino911@gmail.com>
2025-11-23 18:00:55 +0100

    fix: make telescope.nvim's options extensible

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,34 +11,7 @@ return {
   {
     "nvim-telescope/telescope-ui-select.nvim",
     config = function()
-      require("telescope").setup({
-        defaults = {
-          sorting_strategy = "ascending",
-          layout_strategy = "vertical",
-
-          layout_config = {
-            vertical = {
-              prompt_position = "top",
-              mirror = true,
-              results_height = 0.45,
-              preview_height = 0.55,
-            },
-            width = 0.5,
-            height = 0.55,
-          },
-        },
-        extensions = {
-          -- for code actions popup: keep them small!
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({
-              layout_config = {
-                width = 0.40,
-                height = 0.30,
-              },
-            }),
-          },
-        },
-      })
+      require("telescope").setup(require("neovim-idea.options").get_telescope_nvim_options())
       require("telescope").load_extension("ui-select")
 
       -- show line numbers in the preview of Telescope

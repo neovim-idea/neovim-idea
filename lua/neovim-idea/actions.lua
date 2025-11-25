9ffa4d0c27f5307bc3df0674ab13a5972d4444f5
Diego Casella <polentino911@gmail.com>
2025-11-25 21:20:00 +0100

    fix: extracting dap & dapui actions

--- lua/neovim-idea/actions.lua
+++ lua/neovim-idea/actions.lua
@@ -1,5 +1,8 @@
 local Actions = {}
 
+local dap = nil
+local dapui = nil
+
 function Actions.insert_line_above_cursor()
   vim.cmd("normal! O")
   vim.defer_fn(function()
@@ -12,6 +15,27 @@ function Actions.insert_line_below_cursor()
   vim.defer_fn(function()
     vim.cmd("startinsert!")
   end, 10)
+end
+
+function Actions.dap_toggle_breakpoint()
+  assert(dap ~= nil, "dap is nil, did you forget to set it?")
+  dap.toggle_breakpoint()
+end
+
+function Actions.dap_continue()
+  assert(dap ~= nil, "dap is nil, did you forget to set it?")
+  dap.continue()
+end
+
+function Actions.dapui_toggle()
+  assert(dapui ~= nil, "dap is nil, did you forget to set it?")
+  dapui.toggle()
+end
+
+function Actions.setup(opts)
+  dap = opts.dap
+  dapui = opts.dapui
+  return Actions
 end
 
 return Actions

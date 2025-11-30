964adfec47cb7f0b2b3360c447944eb785c8933e
Diego Casella <polentino911@gmail.com>
2025-11-30 12:13:59 +0100

    fix: better rename floating input

--- lua/plugins/snacks-nvim.lua
+++ lua/plugins/snacks-nvim.lua
@@ -1,35 +1,47 @@
 return {
-  "folke/snacks.nvim",
-  priority = 1000,
-  lazy = false,
-  opts = {
-    terminal = { enabled = true },
-    lazygit = { enabled = true },
-  },
-  keys = {
-    {
-      "<D-F12>",
-      function()
-        require("snacks").terminal(nil, { position = "bottom" })
-      end,
-      mode = { "n", "i", "t" },
-      desc = "Toggle Bottom Terminal (Snacks)",
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      terminal = { enabled = true },
+      lazygit = { enabled = true },
+      input = { enabled = true },
     },
-  },
-  config = function()
-    local Snacks = require("snacks")
-    vim.keymap.set({ "n", "i" }, "<D-G>", function()
-      Snacks.lazygit.open()
-    end, { desc = "open lazygit" })
+    keys = {
+      {
+        "<D-F12>",
+        function()
+          require("snacks").terminal(nil, { position = "bottom" })
+        end,
+        mode = { "n", "i", "t" },
+        desc = "Toggle Bottom Terminal (Snacks)",
+      },
+    },
+    config = function(_, opts)
+      require("snacks").setup(opts)
+      local Snacks = require("snacks")
+      vim.keymap.set({ "n", "i" }, "<D-G>", function()
+        Snacks.lazygit.open()
+      end, { desc = "open lazygit" })
 
-    -- fire a neotree event when lazygit closes
-    -- many thanks to https://github.com/nvim-neo-tree/neo-tree.nvim/discussions/1253#discussioncomment-9971975
-    local events = require("neo-tree.events")
-    vim.api.nvim_create_autocmd("TermClose", {
-      pattern = "term://*lazygit*",
-      callback = function()
-        events.fire_event(events.GIT_EVENT)
-      end,
-    })
-  end,
+      -- fire a neotree event when lazygit closes
+      -- many thanks to https://github.com/nvim-neo-tree/neo-tree.nvim/discussions/1253#discussioncomment-9971975
+      local events = require("neo-tree.events")
+      vim.api.nvim_create_autocmd("TermClose", {
+        pattern = "term://*lazygit*",
+        callback = function()
+          events.fire_event(events.GIT_EVENT)
+        end,
+      })
+    end,
+  },
+  {
+    "smjonas/inc-rename.nvim",
+    dependencies = { "folke/snacks.nvim" },
+    lazy = false,
+    config = function()
+      require("inc_rename").setup({ input_buffer_type = "snacks" })
+    end,
+  },
 }

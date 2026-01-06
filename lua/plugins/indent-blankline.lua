db3936567c734209417e3eaf22618dd8611e0040
Diego Casella <polentino911@gmail.com>
2026-01-07 00:50:55 +0100

    fix: cleanup

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -5,7 +5,5 @@ return {
       scope = { enabled = false },
       indent = { char = "│" },
     })
-    -- local palette = require("catppuccin.palettes").get_palette()
-    -- vim.api.nvim_set_hl(0, "MiniIndentscopeSymbol", { fg = palette.surface0 })
   end,
 }

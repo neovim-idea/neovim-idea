1ef3041e1704b2097c974215eb405cbb8e3c9665
Diego Casella <polentino911@gmail.com>
2026-01-07 23:38:07 +0100

    fix: centralising neotree keymaps

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -9,9 +9,5 @@ return {
   lazy = false,
   config = function()
     require("neo-tree").setup(require("neovim-idea.options").get_neotree_options())
-    local actions = require("neovim-idea.actions")
-    vim.keymap.set({ "n", "i" }, "<D-1>", actions.toggle_file_tree, { desc = "toggle Neotree sidebar" })
-    vim.keymap.set({ "n", "i" }, "<D-k1>", actions.toggle_file_tree, { desc = "toggle Neotree sidebar" })
-    vim.keymap.set({ "n", "i" }, "<D-p>", actions.reveal_in_file_tree, { desc = "point in Neotree the current file" })
   end,
 }

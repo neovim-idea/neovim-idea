aba3a0b1d1813bfd191cc2d41941c9878f02be5c
Diego Casella <polentino911@gmail.com>
2026-01-10 00:54:26 +0100

    fix: proper apply overrides

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,14 +1,4 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
-  opts = require("neovim-idea.options").get_which_key_options(),
-  keys = {
-    {
-      "<leader>?",
-      function()
-        require("which-key").show({ global = true})
-      end,
-      desc = "Buffer Local Keymaps (which-key)",
-    },
-  },
 }

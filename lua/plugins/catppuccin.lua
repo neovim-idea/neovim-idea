4786da675a737282dd5591677655fb0df0e850ed
Diego Casella <polentino911@gmail.com>
2026-01-22 22:49:32 +0100

    fix: included catppuccin-reloaded-nvim and docs updates

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,15 +1,8 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
+  "neovim-idea/catppuccin-reloaded-nvim",
+  dependencies = { "catppuccin/nvim" },
   priority = 1000,
   config = function()
-    local catppuccin = require("catppuccin")
-    local path = vim.fn.expand("~/.config/nvim/lua/catppuccin/palettes/")
-    local files = vim.fn.globpath(path, "*", false, true)
-    for i, file in ipairs(files) do
-      catppuccin.flavours[vim.fn.fnamemodify(file, ":t:r")] = 4 + i
-    end
-
-    catppuccin.setup(require("neovim-idea.options").get_catppuccin_options())
+    require("catppuccin-reloaded").setup({ catppuccin = require("neovim-idea.options").get_catppuccin_options() })
   end,
 }
